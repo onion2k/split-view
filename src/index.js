51e6145aaaa7aa76440abac834b218cc4b1a9f5c
Chris Neale <chris@usablehq.com>
2020-09-26 11:36:56 +0100

    Split pos calc in CSS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,71 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+let tmpl = document.createElement('template');
+tmpl.innerHTML = `
+  <style>
+    :host {
+      display: block;
+    }
+    :host([hidden]) { display: none }
+    .split {
+      position: relative;
+      height: 100%;
+      display: grid;
+      align-items: center;
+      --split: 100;
+    }
+    .top,
+    .bottom {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+    .bottom {
+      background-color: red;
+    }
+    .top {
+      z-index: 2;
+      right: calc(8px + (((100% - 16px) / 100) * var(--split)));
+      overflow: hidden;
+      border-right: 1px solid white;
+    }
+    input {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      margin: 0;
+      padding: 0;
+      z-index: 3;
+      background-color: transparent;
+      -webkit-appearance: none;
+    }
+    input[type="range"]:focus {
+      outline: none;
+    }    
+  </style>
+  <div class="split" id="split">
+    <div class="bottom" id="bottom"><slot name="bottom"></slot></div>
+    <div class="top" id="top"><slot name="top"></slot></div>
+    <input type="range" min=0 max=100 value=0 id="slider" />
+  </div>
+`;
+class SplitView extends HTMLElement {
+  constructor() {
+    super();
+    let shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.appendChild(tmpl.content.cloneNode(true));
+  }
+  connectedCallback() {
+    const slider = this.shadowRoot.getElementById("slider");
+    const splitter = this.shadowRoot.getElementById("split");
+    slider.addEventListener("input", (event) => {
+      const split = +event.target.value;
+      console.log(split)
+      splitter.style.setProperty("--split", 100 - split);
+    });
 
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+    const top = this.shadowRoot.getElementById("top");
+    top.style.mixBlendMode = this.getAttribute("mode") || "normal";
+  }
+}
+window.customElements.define('split-view', SplitView);
